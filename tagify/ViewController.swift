9d9a8436bd0b8a1e3a1a3f35c8b37dcdda21a08c
Colin Russell <colinrussell101@gmail.com>
2018-09-18 17:50:40 -0400

    Initial Commit

---
+++ tagify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tagify
+//
+//  Created by Colin on 2018-09-18.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

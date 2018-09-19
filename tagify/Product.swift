68229ac601849b38369317a03dcd914a65b25b2c
Colin Russell <colinrussell101@gmail.com>
2018-09-19 11:56:36 -0400

    added Product struct

---
+++ tagify/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  tagify
+//
+//  Created by Colin on 2018-09-19.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Product {
+    let name: String
+    let description: String
+    let picture: UIImage
+    let tags: [String]
+}

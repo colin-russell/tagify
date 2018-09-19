982becc7c7f8fe4eb117e51e2ee3bd993ee39563
Colin Russell <colinrussell101@gmail.com>
2018-09-18 20:35:05 -0400

    added TagViewController and some basic setup of it

---
+++ tagify/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  tagify
+//
+//  Created by Colin on 2018-09-18.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
--- tagify/TagViewController.swift
+++ tagify/TagViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TagViewController.swift
 //  tagify
 //
 //  Created by Colin on 2018-09-18.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TagViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
